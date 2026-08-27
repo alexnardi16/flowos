36ed3f2e2278e4ac1a5c67bcd2fb5647f57c8fa0
Alex NARDI <90380912+alexnardi16@users.noreply.github.com>
2026-08-27 14:11:05 +0200

    fix: register Android widget without Node require

--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
 import { Platform } from 'react-native';
 
 if (Platform.OS === 'android') {
-  // Keep the Android-only native module out of web/iOS startup.
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { registerWidgetTaskHandler } = require('react-native-android-widget') as typeof import('react-native-android-widget');
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { widgetTaskHandler } = require('./widget-task-handler') as typeof import('./widget-task-handler');
-  registerWidgetTaskHandler(widgetTaskHandler);
+  void import('react-native-android-widget').then(({ registerWidgetTaskHandler }) =>
+    import('./widget-task-handler').then(({ widgetTaskHandler }) => registerWidgetTaskHandler(widgetTaskHandler)),
+  );
 }
 
 import 'expo-router/entry';
