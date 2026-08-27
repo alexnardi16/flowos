9804d946d730fe9117d0d6f6d51301a8520a1561
Alex NARDI <90380912+alexnardi16@users.noreply.github.com>
2026-08-27 18:10:11 +0200

    ci: add explicit Expo ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    rules: {
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/purity': 'off',
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+]);
